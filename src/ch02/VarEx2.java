d9f561f8d27aff1e803a281a78cca15d9119b814
g471000 <g471000@gmail.com>
2022-03-19 14:01:56 +0900

    swap variables

---
+++ src/ch02/VarEx2.java
@@ -0,0 +1,19 @@
+package ch02;
+
+/**
+ * Swap variables
+ */
+public class VarEx2 {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        int temp = 0;
+
+        System.out.println("x: " + x + " y: " + y);
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: " + x + " y: " + y);
+    }
+}
