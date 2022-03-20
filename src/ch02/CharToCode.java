bc0f4035ddfd47cdd4e1f0ea76e59c6139925cc3
g471000 <g471000@gmail.com>
2022-03-20 14:15:40 +0900

    rearrange the format

--- src/ch02/CharToCode.java
+++ src/ch02/CharToCode.java
@@ -3,12 +3,12 @@ package ch02;
 public class CharToCode {
     public static void main(String[] args) {
         char ch = 'A';
-        int code = (int)ch;
+        int code = (int) ch;
 
         System.out.printf("%c = %d(%#X)%n", ch, code, code);
 
         char hch = '천';
-        int hcode = (int)hch;
+        int hcode = (int) hch;
         System.out.printf("%c = %d(%#X)%n", hch, hcode, hcode);
     }
 }
