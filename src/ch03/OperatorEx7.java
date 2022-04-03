656189fb51e0c0d0732772d0030a64d2d0c0fda2
g471000 <g471000@gmail.com>
2022-04-03 18:26:54 +0900

    generating stack overflow which will lead to lose values

---
+++ src/ch03/OperatorEx7.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class OperatorEx7 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 30;
+        byte c = (byte) (a * b);
+
+        // Stack Overflow
+        System.out.println(c);
+    }
+}
