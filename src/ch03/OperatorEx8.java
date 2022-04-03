6201c9fb0b8df92601f50a3bb5ad4eb66f220a79
g471000 <g471000@gmail.com>
2022-04-03 18:30:32 +0900

    stack overflow of using integer

---
+++ src/ch03/OperatorEx8.java
@@ -0,0 +1,13 @@
+package ch03;
+
+public class OperatorEx8 {
+    public static void main(String[] args) {
+        int a = 1_000_000;
+        int b = 2_000_000;
+
+        long c = a * b;
+
+        // Stack Overflow: expected 2,000,000,000,000, but printed negative number due to the overflow
+        System.out.println(c);
+    }
+}
