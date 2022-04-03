b05cf6a2407c34f34b0bb54fec07481e25adb6d1
g471000 <g471000@gmail.com>
2022-04-03 18:25:20 +0900

    casting byte for the a + b which is integer

---
+++ src/ch03/OperatorEx6.java
@@ -0,0 +1,10 @@
+package ch03;
+
+public class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte)(a + b);
+        System.out.println(c);
+    }
+}
