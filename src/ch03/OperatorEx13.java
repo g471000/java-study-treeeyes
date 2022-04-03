5512048756fd72c1d410da3a7f28c8e2dacae3e4
g471000 <g471000@gmail.com>
2022-04-03 18:54:50 +0900

    casting to safely ad char and integer, then store as char

---
+++ src/ch03/OperatorEx13.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class OperatorEx13 {
+    public static void main(String[] args) {
+        char c1 = 'a';
+
+        char c2 = (char) (c1 + 1);
+
+        System.out.println(c2);
+    }
+}
