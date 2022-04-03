f46f10944db41390414a6b3ee091ecb2b5ca2be5
g471000 <g471000@gmail.com>
2022-04-03 22:24:32 +0900

    division to find quotient and remainder

---
+++ src/ch03/OperatorEx19.java
@@ -0,0 +1,10 @@
+package ch03;
+
+public class OperatorEx19 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 8;
+
+        System.out.printf("If we divide %d by %d, quotient is %d and remainder is %d.\n", x, y, x / y, x % y);
+    }
+}
