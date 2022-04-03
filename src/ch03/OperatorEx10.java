34abd672b80b0460e3056db4b571ef121ccb74a2
g471000 <g471000@gmail.com>
2022-04-03 18:40:18 +0900

    multiplication and division order

--- src/ch03/OperatorEx10.java
+++ src/ch03/OperatorEx10.java
@@ -1,4 +1,14 @@
 package ch03;
 
 public class OperatorEx10 {
+    public static void main(String[] args) {
+        int a = 1_000_000;
+
+        int result1 = a * a / a;
+        int result2 = a / a * a;
+
+        // Overflow! Because a * a was calculated earlier, which exceeds the max value of the integer.
+        System.out.printf("%d * %d / %d = %d%n", a, a, a, result1);
+        System.out.printf("%d / %d * %d = %d%n", a, a, a, result2);
+    }
 }
