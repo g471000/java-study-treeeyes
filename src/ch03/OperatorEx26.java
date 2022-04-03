86c7d20944ac61bd845cc11776266293cdd22f92
g471000 <g471000@gmail.com>
2022-04-03 23:20:43 +0900

    operator exercises using || and && with using ++

---
+++ src/ch03/OperatorEx26.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class OperatorEx26 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println(a != 0 && ++b != 0);
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println(a == 0 && ++b != 0);
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
