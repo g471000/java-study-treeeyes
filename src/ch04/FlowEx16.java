e3c04aba84bd4386b1dbd8f95e2f0c3176cb27d9
g471000 <g471000@gmail.com>
2022-04-10 14:27:51 +0900

    print lines of stars

---
+++ src/ch04/FlowEx16.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class FlowEx16 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
