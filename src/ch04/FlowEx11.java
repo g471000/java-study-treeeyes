064c906fde94570c94b3821321687f426b5158c2
g471000 <g471000@gmail.com>
2022-04-10 13:49:06 +0900

    nested switch

--- src/ch04/FlowEx11.java
+++ src/ch04/FlowEx11.java
@@ -1,5 +1,40 @@
 package ch04;
 
-public class FlowEx11 {
+import java.util.Scanner;
 
+public class FlowEx11 {
+    public static void main(String[] args) {
+        System.out.println("Enter your last 7 digits of your Registration Number: ");
+
+        Scanner scan = new Scanner(System.in);
+        String regNo = scan.nextLine();
+        char gender = regNo.charAt(0);
+
+        switch (gender) {
+            case '1':
+            case '3':
+                switch (gender) {
+                    case '1':
+                        System.out.println("You are a male born before 2000.");
+                        break;
+                    case '3':
+                        System.out.println("You are a male born after 2000.");
+                        break;
+                }
+                break;
+            case '2':
+            case '4':
+                switch (gender) {
+                    case '2':
+                        System.out.println("You are a female born before 2000.");
+                        break;
+                    case '3':
+                        System.out.println("You are a male born after 2000");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Not a valid registration number.");
+        }
+    }
 }
