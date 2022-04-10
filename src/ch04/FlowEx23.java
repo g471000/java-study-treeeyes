f2d52ccd5d8d2c5d2c3722c5ae07f173ef8e27f5
g471000 <g471000@gmail.com>
2022-04-10 15:16:59 +0900

    my flow using while loop

---
+++ src/ch04/FlowEx23.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " I love Heize.");
+        }
+    }
+}
