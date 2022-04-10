93eca1ac4c64506b8f3855cc481e72b156511d5d
g471000 <g471000@gmail.com>
2022-04-10 14:09:01 +0900

    using more than 1 variable within for loop

---
+++ src/ch04/FlowEx14.java
@@ -0,0 +1,9 @@
+package ch04;
+
+public class FlowEx14 {
+    public static void main(String[] args) {
+        for (int i = 1, j = 10; i <= 10; i++, j--) {
+            System.out.printf("i = %d \t j = %d\n", i, j);
+        }
+    }
+}
