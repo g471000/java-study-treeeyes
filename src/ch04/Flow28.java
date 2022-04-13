9a9ff9062135dabdefb534d071145c7183e252ad
g471000 <g471000@gmail.com>
2022-04-13 18:18:43 +0900

    guessing number game

---
+++ src/ch04/Flow28.java
@@ -0,0 +1,25 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Flow28 {
+    public static void main(String[] args) {
+        int input = 0, answer = 0;
+
+        answer = (int)(Math.random() * 100) + 1;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Please enter an integer(1 - 100): ");
+            input = scan.nextInt();
+
+            if (input > answer) {
+                System.out.println("Please try with smaller number.");
+            } else if (input < answer) {
+                System.out.println("Please try with larger number.");
+            }
+        } while (input != answer);
+
+        System.out.println("Correct! Answer is: " + answer);
+    }
+}
