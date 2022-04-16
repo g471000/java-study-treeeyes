35e9ebef10859acb379c9367b10d109062dd7baf
g471000 <g471000@gmail.com>
2022-04-16 18:27:49 +0900

    pick from 5 codes to make size 10 array

---
+++ src/ch05/ArrayEx09.java
@@ -0,0 +1,18 @@
+package ch05;
+
+import java.util.Arrays;
+
+public class ArrayEx09 {
+    public static void main(String[] args) {
+        int[] code = {-4, -1, 3, 6, 11};
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            int tmp = (int) (Math.random() * code.length);
+            arr[i] = code[tmp];
+        }
+
+        System.out.println("Code: " + Arrays.toString(code));
+        System.out.println("New Array: " + Arrays.toString(arr));
+    }
+}
