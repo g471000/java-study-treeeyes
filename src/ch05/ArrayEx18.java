db5b7db869c4b6626b4a5f8197395c7538a2dd40
g471000 <g471000@gmail.com>
2022-04-16 21:00:27 +0900

    2d array

---
+++ src/ch05/ArrayEx18.java
@@ -0,0 +1,22 @@
+package ch05;
+
+public class ArrayEx18 {
+    public static void main(String[] args) {
+        int[][] score = {
+                {100, 100, 100},
+                {20, 20, 20},
+                {30, 30, 30},
+                {40, 40, 40}
+        };
+
+        int sum = 0;
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[0].length; j++) {
+                System.out.printf("score[%d][%d]: %d\n", i, j, score[i][j]);
+                sum += score[i][j];
+            }
+        }
+
+        System.out.println("sum: " + sum);
+    }
+}
