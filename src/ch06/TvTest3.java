30e66457b4276af4f2a7d1d974cd60475f3f3919
g471000 <g471000@gmail.com>
2022-04-17 14:48:21 +0900

    change tv reference to another object

---
+++ src/ch06/TvTest3.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class TvTest3 {
+    public static void main(String[] args) {
+        TV tv1 = new TV();
+        TV tv2 = new TV();
+
+        System.out.println("tv1 channel is " + tv1.channel);
+        System.out.println("tv2 channel is " + tv2.channel);
+
+        tv2 = tv1;
+        tv2.channel = 7;
+        System.out.println("changed tv2's channel as " + tv2.channel);
+
+        System.out.println("tv1 channel is " + tv1.channel);
+        System.out.println("tv2 channel is " + tv2.channel);
+    }
+}
