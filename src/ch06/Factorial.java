e564fc4309a8eb065658632527a18a277015f572
g471000 <g471000@gmail.com>
2022-04-18 20:38:26 +0900

    improved factorial using long

--- src/ch06/Factorial.java
+++ src/ch06/Factorial.java
@@ -3,8 +3,21 @@ package ch06;
 public class Factorial {
     public static void main(String[] args) {
         int result = factorial(4);
-
         System.out.println(result);
+
+        // factorial second version
+        int n = 21;
+        long result2 = 0;
+        for (int i = 1; i <= n; i++) {
+            result2 = factorial2(i);
+
+            if(result == -1){
+                System.out.printf("Not Valid. (0 < n <= 20: %d\n", n);
+                break;
+            }
+
+            System.out.printf("%2d! = %20d\n", i, result2);
+        }
     }
 
     static int factorial(int n) {
@@ -12,5 +25,11 @@ public class Factorial {
             return 1;
         }
         return n * factorial(n - 1);
+    }
+
+    static long factorial2(int n) {
+        if(n <= 0 || n > 20) return -1;
+        if(n <= 1) return 1;
+        return n * factorial2(n - 1);
     }
 }
