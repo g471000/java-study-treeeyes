5c4555a91472d6ed24edc5daa64e174c0237eb8c
g471000 <g471000@gmail.com>
2022-04-18 18:12:05 +0900

    math calculate class

---
+++ src/ch06/MyMath.java
@@ -0,0 +1,19 @@
+package ch06;
+
+public class MyMath {
+    long add (long a , long b) {
+        return a + b;
+    }
+
+    long subtract(long a, long b) {
+        return a - b;
+    }
+
+    long multiply(long a, long b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        return a / b;
+    }
+}
