a6253035e629e3907e6f37a87c172ffe9ba5f6ed
g471000 <g471000@gmail.com>
2022-04-18 20:01:53 +0900

    passing reference type which is readable and also writable the variable

--- src/ch06/PrimitiveParamEx.java
+++ src/ch06/PrimitiveParamEx.java
@@ -5,15 +5,26 @@ public class PrimitiveParamEx {
         Data d = new Data();
         d.x = 10;
         System.out.println("main() : x = " + d.x);
+        System.out.println();
 
         change(d.x);
         System.out.println("After call change():");
+        System.out.println("main() : x = " + d.x);
+        System.out.println();
+
+        changeData(d);
+        System.out.println("After call changeData():");
         System.out.println("main() : x = " + d.x);
     }
 
     static void change(int x) {
         x = 1000;
         System.out.println("change(): x = " + x);
+    }
+
+    static void changeData(Data d) {
+        d.x = 1000;
+        System.out.println("changeData(): x = " + d.x);
     }
 }
 
