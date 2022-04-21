b2909ff7986c2dbc077e04796c52387252b45889
g471000 <g471000@gmail.com>
2022-04-21 18:31:26 +0900

    import static members

---
+++ src/ch07/StaticImport.java
@@ -0,0 +1,11 @@
+package ch07;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class StaticImport {
+    public static void main(String[] args) {
+        out.println(random());
+
+        out.println("Math.PI: " + PI);
+    }
+}
