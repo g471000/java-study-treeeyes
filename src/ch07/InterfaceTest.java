cee872d58f08359c63a9e6b05cea98ac6cbd348a
Jieun Chon <g471000@gmail.com>
2022-04-23 21:43:26 +0900

    improved the structure using getInstance method

--- src/ch07/InterfaceTest.java
+++ src/ch07/InterfaceTest.java
@@ -7,6 +7,8 @@ public class InterfaceTest {
 
         a.autoPlay(new B());
         a.autoPlay(new C());
+
+       a.methodA();
     }
 }
 
@@ -15,20 +17,30 @@ class A {
         b.methodB();
     }
 
+    public void methodA() {
+        I i = InstanceManager.getInstance();
+        i.methodB();
+        System.out.println(i.toString());
+    }
+
     void autoPlay(I i) {
         i.play();
     }
 }
 
 
-class B implements I{
+class B implements I {
     public void methodB() {
-        System.out.println("methodB");
+        System.out.println("methodB in B class");
     }
 
     @Override
     public void play() {
         System.out.println("Play in B class");
+    }
+
+    public String toString() {
+        return "class B";
     }
 }
 
@@ -37,8 +49,21 @@ class C implements I {
     public void play() {
         System.out.println("Play in C class");
     }
+
+    @Override
+    public void methodB() {
+        System.out.println("methodB in class C");
+    }
 }
 
 interface I {
     public abstract void play();
+
+    public abstract void methodB();
+}
+
+class InstanceManager {
+    public static I getInstance() {
+        return new B();
+    }
 }
