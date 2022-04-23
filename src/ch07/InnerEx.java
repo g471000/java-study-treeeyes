ac563af4d61e8c13ff97eb4721d56fad95c65d6a
Jieun Chon <g471000@gmail.com>
2022-04-23 21:58:59 +0900

    Inner class and static inner class test

---
+++ src/ch07/InnerEx.java
@@ -0,0 +1,25 @@
+package ch07;
+
+public class InnerEx {
+    class InstanceInner {
+        int iv = 100;
+        final static int CONST = 100;
+    }
+
+    static class StaticInner {
+        int iv = 200;
+        static int cv = 200;
+    }
+
+    void myMethod() {
+        class LocalInner {
+            int iv = 200;
+            final static int CONST = 300;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(InstanceInner.CONST);
+        System.out.println(StaticInner.cv);
+    }
+}
