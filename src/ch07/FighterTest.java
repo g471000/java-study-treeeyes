8f9ba6e7c2bbe08659c2233216ae0d1ac36d7019
Jieun Chon <g471000@gmail.com>
2022-04-23 20:54:07 +0900

    interface and inheritence

---
+++ src/ch07/FighterTest.java
@@ -0,0 +1,46 @@
+package ch07;
+
+public class FighterTest {
+    public static void main(String[] args) {
+        Fighter f = new Fighter();
+
+        if (f instanceof Unit) {
+            System.out.println("f is children of Unit.");
+        }
+        if (f instanceof Fightable) {
+            System.out.println("f is children of Fightable.");
+        }
+        if (f instanceof Movable) {
+            System.out.println("f is children of Movable.");
+        }
+        if (f instanceof Attackable) {
+            System.out.println("f is children of Attackable.");
+        }
+        if (f instanceof Object) {
+            System.out.println("f is children of Object.");
+        }
+    }
+}
+class Fighter extends Unit implements Fightable {
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void attack(Unit unit) {
+        unit.currentHP -= (int) (Math.random() * 20) + 5;
+    }
+}
+
+class Unit {
+    int currentHP;
+    int x, y;
+}
+
+interface Fightable extends Movable, Attackable { }
+interface Movable {
+    void move(int x, int y);}
+
+interface Attackable {
+    void attack(Unit unit);
+}
