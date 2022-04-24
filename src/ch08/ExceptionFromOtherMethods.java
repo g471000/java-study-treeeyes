b8d0a09ed589d5dea40d8c316042db28e57bcb66
Jieun Chon <g471000@gmail.com>
2022-04-24 11:21:14 +0900

    Exception from another method without handling

---
+++ src/ch08/ExceptionFromOtherMethods.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class ExceptionFromOtherMethods {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    static void method1() throws Exception{
+       method2();
+    }
+
+    static void method2() throws Exception {
+        throw new Exception();
+    }
+}
