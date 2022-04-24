1a9be11d005fef2a03713db7117ac82ce6bbb0c0
Jieun Chon <g471000@gmail.com>
2022-04-24 10:54:21 +0900

    showing try-catch flow which skipped catch due to no exception

---
+++ src/ch08/ExceptionFlow.java
@@ -0,0 +1,15 @@
+package ch08;
+
+public class ExceptionFlow {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        try {
+            System.out.println(3);
+            System.out.println(4);
+        } catch (Exception e) {
+            System.out.println(5);
+        }
+        System.out.println(6);
+    }
+}
