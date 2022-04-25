c3b0595494496d7b1b9cf49d8b50feee5a93a42b
Jieun Chon <g471000@gmail.com>
2022-04-25 20:43:03 +0900

    update comparison using equals

--- src/ch09/EqualsEx.java
+++ src/ch09/EqualsEx.java
@@ -10,6 +10,12 @@ public class EqualsEx {
         } else {
             System.out.println("p1 and p2 are different");
         }
+        p2.level = 3;
+        if (p1.equals(p2)) {
+            System.out.println("p1 and p2 are same");
+        } else {
+            System.out.println("p1 and p2 are different");
+        }
 
         p2 = p1;
         if (p1.equals(p2)) {
@@ -22,8 +28,14 @@ public class EqualsEx {
 
 class Pokemon {
     int level;
-
     Pokemon(int level) {
         this.level = level;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Pokemon) {
+            return level == ((Pokemon) obj).level;
+        }
+        return false;
     }
 }
