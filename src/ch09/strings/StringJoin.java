6c813ab933d03903214efa33345c24cbe7191838
Jieun Chon <g471000@gmail.com>
2022-04-25 22:02:06 +0900

    string split and join

---
+++ src/ch09/strings/StringJoin.java
@@ -0,0 +1,19 @@
+package ch09.strings;
+
+import java.util.StringJoiner;
+
+public class StringJoin {
+    public static void main(String[] args) {
+        String animals = "dog,cat,bear";
+        String[] arr = animals.split(",");
+
+        System.out.println(String.join("-", arr));
+
+        StringJoiner sj = new StringJoiner("/", "[", "]");
+        for (String s : arr) {
+            sj.add(s);
+        }
+
+        System.out.println(sj.toString());
+    }
+}
