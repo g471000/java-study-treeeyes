086af04279ec771ef60e8d3588da848e85561e63
Jieun Chon <g471000@gmail.com>
2022-04-25 20:40:21 +0900

    equals test

---
+++ src/ch09/EqualsEx.java
@@ -0,0 +1,29 @@
+package ch09;
+
+public class EqualsEx {
+    public static void main(String[] args) {
+        Pokemon p1 = new Pokemon(5);
+        Pokemon p2 = new Pokemon(5);
+
+        if (p1.equals(p2)) {
+            System.out.println("p1 and p2 are same");
+        } else {
+            System.out.println("p1 and p2 are different");
+        }
+
+        p2 = p1;
+        if (p1.equals(p2)) {
+            System.out.println("p1 and p2 are same");
+        } else {
+            System.out.println("p1 and p2 are different");
+        }
+    }
+}
+
+class Pokemon {
+    int level;
+
+    Pokemon(int level) {
+        this.level = level;
+    }
+}
