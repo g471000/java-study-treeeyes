6fadefcefa2da5e8a88c865b291abe90d2a19455
Jieun Chon <g471000@gmail.com>
2022-04-27 20:41:28 +0900

    scan file and also string with using the delimiter

---
+++ src/ch09/Scanner/ScannerEx3.java
@@ -0,0 +1,27 @@
+package ch09.Scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScannerEx3 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("data3.txt"));
+        int cnt = 0;
+        int totalSum = 0;
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            Scanner sc2 = new Scanner(line).useDelimiter(",");
+            int sum = 0;
+
+            while (sc2.hasNextInt()) {
+                sum += sc2.nextInt();
+            }
+            System.out.println(line + ", sum = " + sum);
+            totalSum += sum;
+            cnt++;
+        }
+        System.out.println("Line: " + cnt + ", Total: " + totalSum);
+    }
+}
