8d6367d0a1dc7b374ee19ec41bd3fe3e245a0828
Jieun Chon <g471000@gmail.com>
2022-04-27 20:17:14 +0900

    grouping the regex result

--- src/ch09/RegexEx1.java
+++ src/ch09/RegexEx1.java
@@ -4,14 +4,22 @@ import java.util.regex.*;
 
 public class RegexEx1 {
     public static void main(String[] args) {
-        String[] data = {"bat", "baby", "bonus", "cA", "ca", "co", "c.", "c0", "car", "combat", "count", "date", "disc"};
-        Pattern p = Pattern.compile("c[a-z]*");
+        String[] data = {"bat", "baby", "bonus", "cA", "ca", "co", "c.", "c0", "car", "combat", "count", "date",
+                "disc"};
+        String[] pattern = {".*", "c[a-z]*", "c[a-z]", "c[a-zA-Z]", "c[a-zA-Z0-9]", "c.", "c.*", "c\\.", "c\\w",
+                "c\\d", "c.*t", "[b|c].*", ".*a.*", ".*a.+", "[b|c].{2}"};
 
-        for (int i = 0; i < data.length; i++) {
-            Matcher m = p.matcher(data[i]);
-            if (m.matches()) {
-                System.out.print(data[i] + ",");
+        System.out.println("pattern matches");
+        for (int i = 0; i < pattern.length; i++) {
+            Pattern p = Pattern.compile(pattern[i]);
+            System.out.printf("Pattern: %10s, Result: ", pattern[i]);
+            for (int j = 0; j < data.length; j++) {
+                Matcher m = p.matcher(data[j]);
+                if (m.matches()) {
+                    System.out.print(data[j] + ",");
+                }
             }
+            System.out.println();
         }
     }
 }
