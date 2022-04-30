62f8e0c4a2610f82b739c09078caa25c562aa9ae
Jieun Chon <g471000@gmail.com>
2022-04-30 11:28:00 +0900

    arraylist methods

---
+++ src/ch11/ArrayListEx1.java
@@ -0,0 +1,51 @@
+package ch11;
+
+import java.util.*;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList  list1 = new ArrayList(10);
+        list1.add(5);
+        list1.add(4);
+        list1.add(2);
+        list1.add(0);
+        list1.add(1);
+        list1.add(3);
+
+        ArrayList list2 = new ArrayList(list1.subList(1, 4));
+        print(list1, list2);
+
+        Collections.sort(list1);
+        Collections.sort(list2);
+        print(list1, list2);
+
+        System.out.println(list1.containsAll(list2));
+
+        list2.add("B");
+        list2.add("C");
+        list2.add(3, "A");
+        print(list1, list2);
+
+        list2.set(3, "AA");
+        print(list1, list2);
+
+        System.out.println(list1.retainAll(list2));
+
+        for (int i = 0; i < list2.size();) {
+            if (list1.contains(list2.get(i))) {
+                list2.remove(i);
+            } else {
+                i++;
+            }
+        }
+
+
+        print(list1, list2);
+    }
+
+    static void print(ArrayList list1, ArrayList list2) {
+        System.out.println("list1: " + list1);
+        System.out.println("list2: " + list2);
+        System.out.println();
+    }
+}
