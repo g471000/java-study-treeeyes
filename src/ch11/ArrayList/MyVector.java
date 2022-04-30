74af1eee818b82b5c78a36ce6d0b80c95a6a63e1
Jieun Chon <g471000@gmail.com>
2022-04-30 12:43:23 +0900

    developed some of vector methods that is herited from the List

---
+++ src/ch11/ArrayList/MyVector.java
@@ -0,0 +1,191 @@
+package ch11.ArrayList;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyVector implements List {
+    Object[] data = null;
+    int capacity = 0;
+    int size = 0;
+    private Object obj;
+
+    public MyVector(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Not valid capacity: " + capacity);
+        }
+        this.capacity = capacity;
+        data = new Object[capacity];
+    }
+
+    public MyVector() {
+        this(10);
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity - data.length > 0) {
+            setCapacity(minCapacity);
+        }
+    }
+
+    public void setCapacity(int capacity) {
+        if (this.capacity == capacity) return;
+
+        Object[] tmp = new Object[capacity];
+        System.arraycopy(data, 0, tmp, 0, size);
+        data = tmp;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public boolean add(Object obj) {
+        ensureCapacity(size + 1);
+        data[size++] = obj;
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void trimToSize() {
+        setCapacity(size);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (obj.equals(o)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bound: " + index);
+        }
+        return data[index];
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bount: " + index);
+        }
+        Object oldObj = data[index];
+
+        if (index != size - 1) {
+            System.arraycopy(data, index + 1, data, index, size - index - 1);
+        }
+
+        data[size - 1] = null;
+        size--;
+        return oldObj;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        Object[] array = new Object[size];
+        System.arraycopy(data, 0, array, 0, size);
+        return array;
+    }
+}
