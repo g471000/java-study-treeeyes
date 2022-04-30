ad9d9ad668b8370afc522eea1ab22da1c7690ae5
Jieun Chon <g471000@gmail.com>
2022-04-30 23:40:04 +0900

    Using ListIterator

---
+++ src/ch11/StackQueue/IteratorAndListIteratorEx1.java
@@ -0,0 +1,40 @@
+package ch11.StackQueue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class IteratorAndListIteratorEx1 {
+
+    static int size = 5;
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < size; i++) {
+            list.add((int) (Math.random() * 10));
+        }
+
+        System.out.println("== Iterator==");
+        Iterator it = list.iterator();
+
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println("== ListIterator ==");
+
+
+        ListIterator lit = list.listIterator();
+        System.out.println("next()...");
+        while (lit.hasNext()) {
+            System.out.print(lit.next() + " ");
+        }
+        System.out.println();
+
+        System.out.println("previous()...");
+        while (lit.hasPrevious()) {
+            System.out.print(lit.previous() + " ");
+        }
+    }
+}
