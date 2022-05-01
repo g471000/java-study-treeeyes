8c44c5c55c5c9500995936c4957a06bbf76e46c2
Jieun Chon <g471000@gmail.com>
2022-05-01 11:23:55 +0900

    basic set example

---
+++ src/ch11/Set/HashSetEx1.java
@@ -0,0 +1,16 @@
+package ch11.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx1 {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", 4, 4, 4, 1};
+        Set set = new HashSet();
+
+        for (int i = 0; i < objArr.length; i++) {
+            set.add(objArr[i]);
+        }
+        System.out.println(set);
+    }
+}
