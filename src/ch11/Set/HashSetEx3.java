0146cf93d7bbd1a40c904a0855ce4cb922038337
Jieun Chon <g471000@gmail.com>
2022-05-01 11:40:43 +0900

    When the set add a object, it calls equals and hashCode, so should override the methods

--- src/ch11/Set/HashSetEx3.java
+++ src/ch11/Set/HashSetEx3.java
@@ -24,6 +24,18 @@ class Person {
         this.age = age;
     }
 
+    public boolean equals(Object o) {
+        if (o instanceof Person) {
+            Person tmp = (Person) o;
+            return name.equals(tmp.name) && age == tmp.age;
+        }
+        return false;
+    }
+
+    public int hashCode() {
+        return (name + age).hashCode();
+    }
+
     public String toString() {
         return name + ":" + age;
     }
