36e2b3fc98bc22a03ca5527fb30847c9b4eea289
Jieun Chon <g471000@gmail.com>
2022-05-02 23:35:06 +0900

    SafeVarargs with SuppressWarning for varargs to not making warnings

---
+++ src/ch12/Annotation/AnnotationEx4.java
@@ -0,0 +1,29 @@
+package ch12.Annotation;
+
+import java.util.Arrays;
+
+class MyArrayList<T> {
+    T[] arr;
+
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    MyArrayList(T... arr) {
+        this.arr = arr;
+    }
+
+    @SafeVarargs
+    public static <T> MyArrayList<T> asList(T... a) {
+        return new MyArrayList<>(a);
+    }
+
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+}
+
+public class AnnotationEx4 {
+    public static void main(String[] args) {
+        MyArrayList<String> list = MyArrayList.asList("1", "2", "3");
+        System.out.println(list);
+    }
+}
