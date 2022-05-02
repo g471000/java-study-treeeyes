14513fa950911b99dc6cf4bd212a214ad0dfab14
Jieun Chon <g471000@gmail.com>
2022-05-02 23:30:50 +0900

    using SuppressWarnings to skip some warnings

--- src/ch12/Annotation/AnnotationEx2.java
+++ src/ch12/Annotation/AnnotationEx2.java
@@ -1,5 +1,7 @@
 package ch12.Annotation;
 
+import java.util.ArrayList;
+
 class NewClass {
     int newField;
 
@@ -17,6 +19,7 @@ class NewClass {
 }
 
 public class AnnotationEx2 {
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         NewClass nc = new NewClass();
 
@@ -24,5 +27,9 @@ public class AnnotationEx2 {
         nc.newField = 10;
         System.out.println(nc.getOldField());
         System.out.println(nc.getNewField());
+
+        @SuppressWarnings("unchecked")
+        ArrayList<NewClass> list = new ArrayList();
+        list.add(nc);
     }
 }
