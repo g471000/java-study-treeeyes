7e9c69878feac5576ccd867bca31a76f8b348e7c
Jieun Chon <g471000@gmail.com>
2022-05-02 23:27:10 +0900

    uses deprecated value

---
+++ src/ch12/Annotation/AnnotationEx2.java
@@ -0,0 +1,27 @@
+package ch12.Annotation;
+
+class NewClass {
+    int newField;
+
+    int getNewField() {
+        return newField;
+    }
+
+    @Deprecated
+    int oldField;
+
+    int getOldField() {
+        return oldField;
+    }
+}
+
+public class AnnotationEx2 {
+    public static void main(String[] args) {
+        NewClass nc = new NewClass();
+
+        nc.oldField = 10;
+        nc.newField = 10;
+        System.out.println(nc.getOldField());
+        System.out.println(nc.getNewField());
+    }
+}
