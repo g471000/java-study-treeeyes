a9417eb21ddc620614c8530dd7c39be7f66b6c60
Jieun Chon <g471000@gmail.com>
2022-05-02 19:02:33 +0900

    update enum

--- src/ch12/Enum/EnumEx1.java
+++ src/ch12/Enum/EnumEx1.java
@@ -1,6 +1,47 @@
 package ch12.Enum;
 
-enum Direction {EAST, SOUTH, WEST, NORTH}
+enum Direction {
+    EAST(1, ">"), SOUTH(2, "V"), WEST(3, "<"), NORTH(4, "^");
+
+
+    private static final Direction[] DIR_ARR = Direction.values();
+    private final int value;
+    private final String symbol;
+
+    Direction(int value, String symbol) {
+        this.value = value;
+        this.symbol = symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public static Direction of(int dir) {
+        if (dir < 1 || dir > 4) {
+            throw new IllegalArgumentException("Invalid value : " + dir);
+        }
+        return DIR_ARR[dir - 1];
+    }
+
+    public Direction rotate(int num) {
+        num = num % 4;
+
+        if (num < 0) {
+            num += 4;
+        }
+
+        return DIR_ARR[(value - 1 + num) % 4];
+    }
+
+    public String toString() {
+        return name() + getSymbol();
+    }
+}
 
 public class EnumEx1 {
     public static void main(String[] args) {
@@ -40,5 +81,17 @@ public class EnumEx1 {
         for (Direction d : dArr) {
             System.out.printf("%s = %d\n", d.name(), d.ordinal());
         }
+
+        System.out.println();
+        Direction d4 = Direction.EAST;
+        Direction d5 = Direction.of(1);
+
+        System.out.printf("d4 = %s, %d\n", d4.name(), d4.getValue());
+        System.out.printf("d5 = %s, %d\n", d5.name(), d5.getValue());
+
+        System.out.println(Direction.EAST.rotate(1));
+        System.out.println(Direction.EAST.rotate(2));
+        System.out.println(Direction.EAST.rotate(-1));
+        System.out.println(Direction.EAST.rotate(-2));
     }
 }
