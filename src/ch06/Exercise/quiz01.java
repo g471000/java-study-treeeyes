f3bb9dd4ff7f65d7e790121adb32c8db7c19c040
JIEUN CHON <g471000@gmail.com>
2022-05-05 08:02:37 +0900

    create simple sutda class

---
+++ src/ch06/Exercise/quiz01.java
@@ -0,0 +1,17 @@
+package ch06.Exercise;
+
+/**
+ * Create a class with below condition:
+ *
+ * int num, card number (1 - 10, integer)
+ * boolean isKwang (if it is kwang, it is true. Otherwise false)
+ */
+public class quiz01 {
+
+}
+
+class SutdaCard{
+    int num;
+    boolean isKwang;
+
+}
