b517430bf8e8f7ea5b6f6727bc4a45a0a8c15206
JIEUN CHON <g471000@gmail.com>
2022-05-05 08:08:38 +0900

    create Student class

---
+++ src/ch06/Exercise/Student.java
@@ -0,0 +1,10 @@
+package ch06.Exercise;
+
+public class Student {
+    String name;
+    int classNum;
+    int studentNum;
+    int korScore;
+    int engScore;
+    int mathScore;
+}
---
+++ src/ch06/Exercise/quiz03.java
@@ -0,0 +1,5 @@
+package ch06.Exercise;
+
+public class quiz03 {
+    // See Student.java
+}
