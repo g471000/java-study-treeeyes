7679e0d60219c53fedc43c9ac1b760365996f0c1
JIEUN CHON <g471000@gmail.com>
2022-05-06 12:17:10 +0900

    count down using JOptionPane and sleep the thread for 1 sec

---
+++ src/ch13/ThreadEx06.java
@@ -0,0 +1,20 @@
+package ch13;
+
+import javax.swing.*;
+import javax.xml.catalog.Catalog;
+
+public class ThreadEx06 {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Insert any value.");
+        System.out.println("You entered " + input);
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
