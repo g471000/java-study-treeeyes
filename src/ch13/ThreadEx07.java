4584ff80ef7a5033674ae9b79770471dd2a754da
JIEUN CHON <g471000@gmail.com>
2022-05-06 12:23:07 +0900

    run the second counting before the inputs, because using differnt thread

--- src/ch13/ThreadEx07.java
+++ src/ch13/ThreadEx07.java
@@ -2,11 +2,18 @@ package ch13;
 
 import javax.swing.*;
 
-public class ThreadEx06 {
+public class ThreadEx07 {
     public static void main(String[] args) {
+        Thread07 t = new Thread07();
+        t.start();
+
         String input = JOptionPane.showInputDialog("Insert any value.");
         System.out.println("You entered " + input);
+    }
+}
 
+class Thread07 extends Thread {
+    public void run() {
         for (int i = 10; i > 0; i--) {
             System.out.println(i);
             try {
