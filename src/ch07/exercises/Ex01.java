a85d2dc2dd209305bd867dd2b0de3c6b03b31965
Jieun Chon <g471000@gmail.com>
2022-05-19 18:06:52 +0900

    generate sutda card deck

---
+++ src/ch07/exercises/Ex01.java
@@ -0,0 +1,49 @@
+package ch07.exercises;
+
+import ch06.Card;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        SutdaDeck deck = new SutdaDeck();
+
+        for (int i = 0; i < deck.cards.length; i++) {
+            System.out.print(deck.cards[i] + ",");
+        }
+    }
+}
+
+class SutdaDeck {
+    final int CARD_NUM = 20;
+    SutdaCard[] cards = new SutdaCard[CARD_NUM];
+
+    SutdaDeck() {
+        for (int i = 0; i < cards.length; i++) {
+            int num = i % 10 + 1;
+            boolean isKwang = (i < 10) && isOneThreeEight(num);
+
+            cards[i] = new SutdaCard(num, isKwang);
+        }
+    }
+
+    private boolean isOneThreeEight(int num) {
+        return num == 1 || num == 3 || num == 8;
+    }
+}
+
+class SutdaCard {
+    int num;
+    boolean isKwang;
+
+    SutdaCard() {
+        this(1, true);
+    }
+
+    SutdaCard(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    public String toString() {
+        return num + (isKwang ? "K" : "");
+    }
+}
